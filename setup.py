a0228b63e3f141d05e922bc3e479a665c1f2e646
Abhay Singh <singhabhay2362@gmail.com>
2023-04-27 00:18:59 +0530

    Files upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ClinicManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Abhay',
+    author_email='singhabhay2362@gmail.com',
+    description='Simple Clinic Management System Project'
+)
